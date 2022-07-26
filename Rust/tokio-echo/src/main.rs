2c6ef1cc34def16bbf64fc4531aaa08452c5b73f
Nathaniel McCallum <nathaniel@profian.com>
2022-07-26 06:30:15 -0400

    feat: add tokio-echo
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

---
+++ Rust/tokio-echo/src/main.rs
@@ -0,0 +1,46 @@
+use std::io::Write;
+use std::os::wasi::prelude::FromRawFd;
+
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main(flavor = "current_thread")]
+
+async fn main() -> std::io::Result<()> {
+    // Set up pre-established listening socket.
+    let standard = unsafe { std::net::TcpListener::from_raw_fd(3) };
+    standard.set_nonblocking(true).unwrap();
+    let listener = TcpListener::from_std(standard)?;
+
+    loop {
+        // Accept new sockets in a loop.
+        let (socket, _) = listener.accept().await?;
+
+        // Spawn a background task for each new connection.
+        tokio::spawn(async move {
+            eprintln!("> CONNECTED");
+            match handle(socket).await {
+                Ok(()) => eprintln!("> DISCONNECTED"),
+                Err(e) => eprintln!("> ERROR: {}", e),
+            }
+        });
+    }
+}
+
+async fn handle(mut socket: TcpStream) -> std::io::Result<()> {
+    loop {
+        let mut buf = [0u8; 4096];
+
+        // Read some bytes from the socket.
+        let read = socket.read(&mut buf).await?;
+
+        // Handle a clean disconnection.
+        if read == 0 {
+            return Ok(());
+        }
+
+        // Write bytes both locally and remotely.
+        std::io::stdout().write_all(&buf[..read])?;
+        socket.write_all(&buf[..read]).await?;
+    }
+}
