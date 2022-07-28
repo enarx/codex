0b78ddca05d5cba2adbefa747089d09bb9a19960
Richard Zak <richard@profian.com>
2022-07-28 12:30:00 -0400

    fixup: CLI arguments for C, CPP, Go, Ruby
    Signed-off-by: Richard Zak <richard@profian.com>

--- C++/fibonacci/fibonacci.cpp
+++ C++/fibonacci/fibonacci.cpp
@@ -1,16 +1,25 @@
-// Simple Program to calculate Fibonacci Sequence of an integer input
 #include <iostream>
+#include <cstdlib>
 using namespace std;
-int FibonacciSequence(int num) {
+
+unsigned int FibonacciSequence(unsigned int num) {
     if(num <= 1) {
         return num ;
     }
     return FibonacciSequence(num-1) + FibonacciSequence(num-2);
 }
-int main(){
-    cout << "Enter the Number" << endl;
-    int n ;
-    cin  >> n ;
 
-    cout << "Fibonacci Sequence term at " << n << "  " << "is " << FibonacciSequence(n) << endl;
+int main(int argc, char* argv[]){
+	unsigned int n;
+	if (argc > 1) {
+		for(int i = 1; i < argc; i++) {
+			n = atoi(argv[i]);
+			cout << "Fibonacci sequence number at index " << n << " is " << FibonacciSequence(n) << endl;
+		}
+	} else {
+		cout << "Which Fibonacci index to find? ";
+		cout.flush();
+		cin >> n;
+		cout << "Fibonacci sequence number at index " << n << " is " << FibonacciSequence(n) << endl;
+	}
 }
--- C/fibonacci/fibonacci.c
+++ C/fibonacci/fibonacci.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int FibonacciSequence(int num) {
+unsigned int FibonacciSequence(unsigned int num) {
     if(num <= 1) {
         return num ;
     }
     return FibonacciSequence(num-1) + FibonacciSequence(num-2);
 }
-int main(){
-    printf("Enter the Number\n");
-    int n ;
-    scanf("%d",&n);
 
-    printf("Fibonacci Sequence term at %d is %d " , n , FibonacciSequence(n));
+int main(int argc, char* argv[]){
+	unsigned int n;
+    if (argc > 1) {
+    	int i;
+    	for(i = 1; i < argc; i++) {
+    		n = atoi(argv[i]);
+    		printf("Fibonacci sequence number at index %d is %d\n" , n , FibonacciSequence(n));
+    	}
+    } else {
+    	printf("Which Fibonacci index to find? ");
+    	int matched = scanf("%u", &n);
+    	if (matched < 1) {
+    		n = 10;
+    	}
+    	printf("Fibonacci sequence number at index %d is %d\n" , n , FibonacciSequence(n));
+    }
 }
