c9f50ded8d8858f1f1f2db041a7e0f39c2b15b49
bstrie <865233+bstrie@users.noreply.github.com>
2022-07-28 17:08:19 -0400

    feat: make Python fibonacci example take arguments
    Signed-off-by: bstrie <865233+bstrie@users.noreply.github.com>

--- Python/fibonacci/fib.py
+++ Python/fibonacci/fib.py
@@ -1,15 +1,20 @@
-nterms = 10
-n1, n2 = 0, 1
-count = 0
-if nterms == 1:
-   print("Fibonacci sequence upto",nterms,":")
-   print(n1)
+from functools import cache
+import sys
+
+@cache
+def fib(n):
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+args = sys.argv[1:]
+
+if len(args) == 0:
+    print("Please pass one or more numbers as arguments to the program")
 else:
-   print("Fibonacci sequence:")
-   while count < nterms:
-       print(n1)
-       nth = n1 + n2
-       # update values
-       n1 = n2
-       n2 = nth
-       count += 1
+    for arg in args:
+        idx = int(arg)
+        print(f"Fibonacci sequence number at index {idx} is {fib(idx)}")
