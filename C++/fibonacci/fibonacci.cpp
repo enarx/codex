8c274808d34e6e8eb36ff6f4c95da3b1b8071499
Roman Volosatovs <roman@profian.com>
2022-08-02 11:29:42 +0200

    fix(c++/fib): ensure example consistency
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- C++/fibonacci/fibonacci.cpp
+++ C++/fibonacci/fibonacci.cpp
@@ -1,25 +1,34 @@
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
+#include <string.h>
+
 using namespace std;
 
-unsigned int FibonacciSequence(unsigned int num) {
-    if(num <= 1) {
-        return num ;
-    }
-    return FibonacciSequence(num-1) + FibonacciSequence(num-2);
+unsigned long fib(unsigned long i) {
+  if (i <= 1) {
+    return i;
+  }
+  return fib(i - 1) + fib(i - 2);
 }
 
-int main(int argc, char* argv[]){
-	unsigned int n;
-	if (argc > 1) {
-		for(int i = 1; i < argc; i++) {
-			n = atoi(argv[i]);
-			cout << "Fibonacci sequence number at index " << n << " is " << FibonacciSequence(n) << endl;
-		}
-	} else {
-		cout << "Which Fibonacci index to find? ";
-		cout.flush();
-		cin >> n;
-		cout << "Fibonacci sequence number at index " << n << " is " << FibonacciSequence(n) << endl;
-	}
+int main(int argc, char *argv[]) {
+  if (argc <= 1) {
+    unsigned long n;
+    cout << "Enter a non-negative number:" << endl;
+    cin >> n;
+    cout << "Fibonacci sequence number at index " << n << " is " << fib(n)
+         << endl;
+  } else {
+    for (unsigned int i = 1; i < argc; i++) {
+      errno = 0;
+      unsigned long n = strtoul(argv[i], NULL, 10);
+      if (errno != 0) {
+        cerr << "Failed to parse argument into a number: " << strerror(errno)
+             << endl;
+        exit(1);
+      }
+      cout << "Fibonacci sequence number at index " << n << " is " << fib(n)
+           << endl;
+    }
+  }
 }
