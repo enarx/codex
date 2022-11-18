2415afe78a783ac3079f2de8c6325465534e223a
Udochukwu Nwosu <unwosu6@jhu.edu>
2022-11-18 09:54:12 -0500

    feat(chat-client-c): support local execution
    Signed-off-by: Udochukwu Nwosu <unwosu6@jhu.edu>

--- demos/chat-client/c/main.c
+++ demos/chat-client/c/main.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
- 
+
+#ifndef __wasi__
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#endif
+
 int main(int argc, char const* argv[]) {
-    int fd_count = atoi(getenv("FD_COUNT"));
+/*    int fd_count = atoi(getenv("FD_COUNT"));
     if (fd_count != 4) {
         fprintf(stderr, "unexpected amount of file descriptors received:%d\n", fd_count);
         return 1;
@@ -21,12 +26,30 @@ int main(int argc, char const* argv[]) {
     } else if (strcmp(stream_str, "client") != 0) {
         fprintf(stderr, "unknown socket name %s", stream_str);
     }
- 
+ */
     char line[1024];
  
     // TODO: Send and receive multiple messages concurrently once async reads from stdin are possible
     int lineRead, lineSent, numBytes;
     int stream = 3;
+#ifndef __wasi__
+    int client_fd = 0, sock = 0;
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(50000);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("Invalid address/ Address not supported \n");
+        return -1;
+    }
+ 
+    if ((client_fd = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    stream = client_fd;
+#endif
     char c;
     while (1) {
         c = '\0';
